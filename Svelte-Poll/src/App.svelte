27454428f04190e1b3cb5c113abbac5bceec7b29
Dominik Croci <dominikrcroci@gmail.com>
2023-05-10 14:50:41 -0700

    footer component

--- Svelte-Poll/src/App.svelte
+++ Svelte-Poll/src/App.svelte
@@ -1,17 +1,11 @@
 <script>
   import Header from './components/header.svelte';
+  import Footer from './components/footer.svelte';
 </script>
 
 <Header />
-<main>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi voluptate
-    quis hic facere explicabo non. Eligendi id maxime cupiditate vitae debitis,
-    eos est? Laborum maiores eveniet in distinctio, et provident cupiditate
-    earum, placeat voluptate, corporis dolores! Eius tempore quam quibusdam,
-    quaerat dolores veniam et facilis aliquam placeat consequuntur dolorem illo!
-  </p>
-</main>
+<main />
+<Footer />
 
 <style>
   main {
