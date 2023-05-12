469ce539e3964916dee3842963a4b31c4b408e7c
Dominik Croci <dominikrcroci@gmail.com>
2023-05-11 20:31:43 -0700

    tab update

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,18 @@
 <script>
   import Header from './components/header.svelte';
   import Footer from './components/footer.svelte';
+  import Tabs from './components/shared/Tabs.svelte';
+  let items = ['Current Polls', 'Create New Poll'];
+  let activeItem = 'Current Polls';
+  const tabChange = (e) => {
+    activeItem = e.detail;
+  };
 </script>
 
 <Header />
-<main />
+<main>
+  <Tabs {activeItem} {items} on:tabchange={tabChange} />
+</main>
 <Footer />
 
 <style>
